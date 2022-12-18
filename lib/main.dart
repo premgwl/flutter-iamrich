245599cd1308f9ad29fbf963e74b4e76bb689ab3
premgwl <premgupta470@gmail.com>
2022-12-19 02:21:08 +0530

    Add image to scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,14 @@ void main() {
           title: const Text('I Am Rich'),
           backgroundColor: Colors.blueGrey,
         ),
+        backgroundColor: Colors.indigo,
+        // ignore: prefer_const_constructors
+        body: Center(
+          child: const Image(
+            image: NetworkImage(
+                'https://i.insider.com/52124584ecad041839000018?width=750&format=jpeg&auto=webp'),
+          ),
+        ),
       ),
     ),
   );
